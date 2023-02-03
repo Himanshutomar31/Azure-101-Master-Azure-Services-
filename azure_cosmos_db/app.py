5e8d4667af5ee1d84858ce9c555ad4a3f4d15b6b
Himanshu Tomar <himanshutomar3031@gmail.com>
2023-02-03 22:41:30 +0530

    Added logic for CRUD cosmos db

--- azure_cosmos_db/app.py
+++ azure_cosmos_db/app.py
@@ -1,79 +1,48 @@
-
 import pandas as pd 
 import json
-import azure.cosmos.cosmos_client as cosmos_client
-import azure.cosmos.errors as errors
-import azure.cosmos.documents as documents
-import azure.cosmos.http_constants as http_constants
+import psycopg2
+
 
 print('Imported packages successfully.')
 
-# Initialize the Cosmos client
-
-config = {
-    "endpoint": "YOUR ENDPOINT HERE",
-    "primarykey": "YOUR PRIMARY KEY HERE"
-}
-
-# Create the cosmos client
-client = cosmos_client.CosmosClient(url_connection=config["endpoint"], auth={"masterKey":config["primarykey"]}
-)
-
-# Create a database
-# https://docs.microsoft.com/en-us/python/api/azure-cosmos/azure.cosmos.cosmos_client.cosmosclient?view=azure-python#createdatabase-database--options-none-
-
-database_name = 'posgres-az204'
 try:
-    database = client.CreateDatabase({'id': database_name})
-except errors.HTTPFailure:
-    database = client.ReadDatabase("dbs/" + database_name)
+    conn_string="host=c.demo-cosmos-db-204.postgres.database.azure.com port=5432 dbname=citus user=citus password=cosmos@123 sslmode=require"
+    conn = psycopg2.connect(conn_string)
+except Exception as exp:
+    print(exp)
 
-# Create a collection of items in a Container
-# Items are the individual rows/records of your dataset
-# https://docs.microsoft.com/en-us/python/api/azure-cosmos/azure.cosmos.cosmos_client.cosmosclient?view=azure-python#createcontainer-database-link--collection--options-none-
+print("successfully connected")
 
-database_link = 'dbs/' + 'posgres-az204'
-container_definition = {'id': 'HDIcontainer',
-                        'partitionKey':
-                                    {
-                                        'paths': ['/country'],
-                                        'kind': documents.PartitionKind.Hash
-                                    }
-                        }
-try:
-    container = client.CreateContainer(database_link=database_link, 
-                                        collection=container_definition, 
-                                        options={'offerThroughput': 400})
-except errors.HTTPFailure as e:
-    if e.status_code == http_constants.StatusCodes.CONFLICT:
-        container = client.ReadContainer("dbs/" + database['id'] + "/colls/" + container_definition['id'])
-    else:
-        raise e
+cursor = conn.cursor()
 
+cursor.execute("Drop table if exists countries")
 
 # Download and read csv file
 df = pd.read_csv('https://globaldatalab.org/assets/2019/09/SHDI%20Complete%203.0.csv',encoding='ISO-8859–1',dtype='str')
-# Reset index - creates a column called 'index'
 df = df.reset_index()
-# Rename that new column 'id'
-# Cosmos DB needs one column named 'id'. 
 df = df.rename(columns={'index':'id'})
-# Convert the id column to a string - this is a document database.
 df['id'] = df['id'].astype(str)
 
+#create statement for ccountries table
+sql_text = pd.io.sql.get_schema(df, "countries")   
+print(sql_text)
 
-# https://docs.microsoft.com/en-us/python/api/azure-cosmos/azure.cosmos.cosmos_client.cosmosclient?view=azure-python#upsertitem-database-or-container-link--document--options-none-
-# Write rows of a pandas DataFrame as items to the Database Container
+#create table in the database
+cursor.execute(sql_text)
 
-# Create Connection Link string
-collection_link = database_link + '/colls/' + 'HDIcontainer'
+res=cursor.execute("SELECT table_name, column_name, data_type FROM information_schema.columns")
+print(res)
 
-# Write rows of a pandas DataFrame as items to the Database Container
-for i in range(0,df.shape[0]):
-    # create a dictionary for the selected row
-    data_dict = dict(df.iloc[i,:])
-    # convert the dictionary to a json object.
-    data_dict = json.dumps(data_dict)
-    insert_data = client.UpsertItem(collection_link,json.loads(data_dict)
-    )
+# #insert data into posgres table
+# for index, row in df.iterrows():       
+#     insert_query = 'INSERT INTO countries ('+ str(', '.join(df.columns))+ ') VALUES '+ str(tuple(row.values))
+#     print(insert_query)
+#     cursor.execute(insert_query)     
+
+#Fetch all rows from table
+cursor.execute("Select * from countries where country like 'india%';")
+rows = cursor.fetchall()
+print("Table data")
+for row in rows:
+    print(row)
 print('Records inserted successfully.')
