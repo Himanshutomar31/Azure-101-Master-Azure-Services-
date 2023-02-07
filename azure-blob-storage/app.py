b82b315380163dbd52b8d2a4d6eea19ea9c4f596
Himanshu Tomar <himanshutomar3031@gmail.com>
2023-02-07 12:21:08 +0530

    Added a cond to check whether container exists or not

--- azure-blob-storage/app.py
+++ azure-blob-storage/app.py
@@ -9,8 +9,8 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 def create_container(blob_service_client):
     try:
-        container_name = "az_204_blob_storage"
-        container_client = blob_service_client.create_container()
+        container_name = "az-204-blob-storage"
+        container_client = blob_service_client.create_container(container_name)
     except Exception as exp:
         raise exp
 
@@ -21,8 +21,12 @@ try:
     default_credential = DefaultAzureCredential()
 
     blob_service_client = BlobServiceClient(account_url, credential=default_credential)
-
-    create_container(blob_service_client)
+    container_client = blob_service_client.get_container_client("az-204-blob-storage")
+    if container_client.exists():
+        print("Container exists")
+    else:
+        print("Not Found")
+        create_container(blob_service_client)
 except Exception as exp:
     print(exp)
     
